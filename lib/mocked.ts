26daa713db18dc932cf885bc9c591f48028a7f40
HyunTaek Oh <oht222@naver.com>
2025-01-12 03:32:03 +0900

    fix: 모킹 데이터 import 위치 수정

---
+++ lib/mocked.ts
@@ -0,0 +1,32 @@
+export const memberList = [
+  {
+    id: 1,
+    position: 'Front-end',
+    comment: '마이페이지 상태관리 구현 중...',
+    image: '/member-icon/member1.png'
+  },
+  {
+    id: 2,
+    position: 'Front-end',
+    comment: '버튼 컴포넌트 제작 중..',
+    image: '/member-icon/member2.png'
+  },
+  {
+    id: 3,
+    position: 'Back-end',
+    comment: '서버 배포중..',
+    image: '/member-icon/member3.png'
+  },
+  {
+    id: 4,
+    position: 'Back-end',
+    comment: '데이터베이스 구축 중..',
+    image: '/member-icon/member4.png'
+  },
+  {
+    id: 5,
+    position: 'Designer',
+    comment: '로고 디자인 중..',
+    image: '/member-icon/member5.png'
+  }
+];
